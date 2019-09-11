ec2556ff6ab82eaf526f2f59eb6d0791f6653598
FrenchMan124 <55214313+FrenchMan124@users.noreply.github.com>
2019-09-12 10:10:25 +1200

    Add files via upload

--- movie_reccomendation.py
+++ movie_reccomendation.py
@@ -50,3 +50,54 @@ class User:
 
     def return_disliked(self):
         return self.disliked
+
+def import_movies():
+    """ Load movie csv files movies class """
+    import csv
+    with open('movies.csv', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0: # Header row
+                print('Column names are {}'.format(", ".join(row)))
+                line_count += 1
+            else:
+                line_count += 1
+
+                """*** Add the imported data to the Movie class ***"""
+                # Seperating the year and title
+                year = (row[1])[-5:-1]
+                title = (row[1])[0:-6]
+                id = (row[0])
+                
+                # ***Do some string processing to import into the Movie class ***
+                Movies(row[0], year, title, row[2])
+                
+        print('Movies, Processed {} lines.'.format(line_count))
+
+
+
+
+
+if __name__ == "__main__":
+    LIKED_RATING = "4"  # Movies rated this score and above are considered liked
+    movies = []     # Stores all instances of movies
+    ratings = []    # Stores all instances of ratings
+    users = []      # Stores all instances of users
+
+    # Import csv as objects
+    import_movies()
+    import_ratings(LIKED_RATING)
+
+    similarity_index(CURRENT_USER, users[5])
+    # Checking
+    print("\nLiked:\n", users[0].return_liked())
+    print("\nDisliked:\n", users[0].return_disliked())
+
+    # Set the current user and number of recommendations
+    current_user_id = '1'
+    num_of_recommendations = 5
+
+    # Store current user instance
+    CURRENT_USER = set_current_user(current_user_id)
+    
